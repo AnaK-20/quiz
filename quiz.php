6c27b26d898b454fe87f666aa1dc54d5449b39c6
Ana Karolina <anak54497@gmail.com>
2023-05-29 16:20:57 -0300

    Ínicio do projeto

---
+++ quiz.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Principais Escritores Brasileiros</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='quiz.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+    <?php
+        $início = 
+        $quiz = array(
+            $q1 = array('r1', 'r2', 'r3', 'r4', 'correta' );
+            $q2 = array('r1', 'r2', 'r3', 'r4', 'correta' );
+            $q3 = array('r1', 'r2', 'r3', 'r4', 'correta' );
+            $q4 = array('r1', 'r2', 'r3', 'r4', 'correta' );
+            $q5 = array('r1', 'r2', 'r3', 'r4', 'correta' );
+            $q6 = array('r1', 'r2', 'r3', 'r4', 'correta' );
+            $q7 = array('r1', 'r2', 'r3', 'r4', 'correta' );
+            $q8 = array('r1', 'r2', 'r3', 'r4', 'correta' );
+            $q9 = array('r1', 'r2', 'r3', 'r4', 'correta' );
+            $q10 = array('r1', 'r2', 'r3', 'r4', 'correta' );
+        );
+
+    ?>
+    <form action="quiz.php" method="post">
+        <table></table>
+    </form>
+    
+</body>
+</html>
