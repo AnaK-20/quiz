ddfcf451e7961ea1148f205b85687f4e9edb398f
Ana Karolina <anak54497@gmail.com>
2023-06-02 15:05:30 -0300

    Atualização 1

--- quiz.php
+++ quiz.php
@@ -10,9 +10,15 @@
 </head>
 <body>
     <?php
-        $início = 
+        $inicio = array('Título', 'Digite seu nome:', $_POST['nome'], 'Jogar', 'Responder', 'Continuar', 'Reiniciar');
         $quiz = array(
-            $q1 = array('r1', 'r2', 'r3', 'r4', 'correta' );
+            $q1 = array('1- Quem é a autora que recebeu ao nascer o nome Chaya Pinkhasovna( posteriormente o mudou) teve que fugir para o Brasil devido ao 
+            aumento de antissemitismo em seu país de origem e aos  aos 10 anos compôs sua primeira peça para piano em homenagem a sua mãe.
+             Além de ter se formado em direito na faculdade do Rio de Janeiro e ter trabalhado no ramo jornalístico? ',
+             'Cecília  Meireles(1901-1964) - autora da obra “Romanceiro da Inconfidência”.',
+             'Rachel de Queiroz(1910-2003) - autora da obra “O quinze”.', 
+             'Clarice Lispector(1920-1977) - autora de “Perto do Coração Selvagem”.',
+             'Cora Coralina (1889-1985)- autora de “Todas as Vidas”.', 'Clarice Lispector(1920-1977 )- autora de “Perto do Coração Selvagem”.' );
             $q2 = array('r1', 'r2', 'r3', 'r4', 'correta' );
             $q3 = array('r1', 'r2', 'r3', 'r4', 'correta' );
             $q4 = array('r1', 'r2', 'r3', 'r4', 'correta' );
@@ -26,7 +32,19 @@
 
     ?>
     <form action="quiz.php" method="post">
-        <table></table>
+        <table>
+            <tr>
+                <td colspan="2">
+                    <?php echo "<h2>".$inicio[0]."</h2>" ?>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <label for="nome"><?php echo $inicio[1] ?></label>
+                    <input type="text" name="nome" value="<?php  echo $inicio[2]?>">
+                </td>
+            </tr>
+        </table>
     </form>
     
 </body>
