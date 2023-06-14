d60c5419a636a50cc1dcc4ad68e92876f0b8df6e
Ana Karolina <anak54497@gmail.com>
2023-06-14 15:37:51 -0300

    Atualização 8

--- teste2.php
+++ teste2.php
@@ -16,100 +16,100 @@
                 aumento de antissemitismo em seu país de origem e aos  aos 10 anos compôs sua primeira peça para piano em homenagem a sua mãe.
                 Além de ter se formado em direito na faculdade do Rio de Janeiro e ter trabalhado no ramo jornalístico? ',
                 'opcoes'=> array(
-                'a'=>'Cecília Meireles(1901-1964) - autora da obra “Romanceiro da Inconfidência”.',
-                'b'=>'Rachel de Queiroz(1910-2003) - autora da obra “O quinze”.', 
-                'c'=>'Clarice Lispector(1920-1977) - autora de “Perto do Coração Selvagem”.',
-                'd'=>'Cora Coralina(1889-1985) - autora de “Todas as Vidas”.'), 
-                'resposta' => 'c'
+                'Cecília Meireles(1901-1964) - autora da obra “Romanceiro da Inconfidência”.',
+                'Rachel de Queiroz(1910-2003) - autora da obra “O quinze”.', 
+                'Clarice Lispector(1920-1977) - autora de “Perto do Coração Selvagem”.',
+                'Cora Coralina(1889-1985) - autora de “Todas as Vidas”.'), 
+                'resposta' => 'Clarice Lispector(1920-1977) - autora de “Perto do Coração Selvagem”.'
             ),
             array(
                 'pergunta'=>'Tendo como romance mais famoso a obra “Ponciá Vicêncio”, essa autora não foi só escritora como também professora e suas  
                 obras são caracterizadas, em maior parte, pelo protagonismo mulheres negras no contexto da temática de discriminação racial. <br/>',
                 'opcoes'=> array(
-                'a'=>'Adélia Prado',
-                'b'=>'Conceição Evaristo', 
-                'c'=>'Maria Firmina dos Reis',
-                'd'=>'Carolina Maria de Jesus'), 
-                'resposta' => 'b'
+                'Adélia Prado',
+                'Conceição Evaristo', 
+                'Maria Firmina dos Reis',
+                'Carolina Maria de Jesus'), 
+                'resposta' => 'Conceição Evaristo'
             ),
             array(
                 'pergunta'=>'Este autor inspirou algumas músicas como “Até o fim”  e “Flor da Idade” do compositor brasileiro Chico Buarque e teve o seu poema denominado 
                 “A Máquina do Mundo” escolhido  por um grupo significativo de escritores e críticos como o melhor poema brasileiro de todos os tempos. Atualmente, 
                 sua obra mais importante é  a“ Rosa do Povo”(1945). <br/>',
                 'opcoes'=> array(
-                'a'=>'José de Alencar',
-                'b'=>'Mário Quintana', 
-                'c'=>'Jorge Amado',
-                'd'=>'Carlos Drummond de Andrade'), 
-                'resposta' => 'd'
+                'José de Alencar',
+                'Mário Quintana', 
+                'Jorge Amado',
+                'Carlos Drummond de Andrade'), 
+                'resposta' => 'Carlos Drummond de Andrade'
             ),
             array(
                 'pergunta'=>'Este é o autor brasileiro com mais adaptações para a tv e cinema, seu livro “Cacau” ganhou destaque ao ser apreendido pela polícia política de Getúlio Vargas,
                  a temática dos seus livros percorrem histórias reais da realidade das pessoas daquela época. <br/>',
                 'opcoes'=> array(
-                'a'=>'Mário Quintana',
-                'b'=>'Jorge Amado', 
-                'c'=>'Machado de Assis',
-                'd'=>'Carlos Drummond de Andrade'), 
-                'resposta' => 'b'
+                'Mário Quintana',
+                'Jorge Amado', 
+                'Machado de Assis',
+                'Carlos Drummond de Andrade'), 
+                'resposta' => 'Jorge Amado'
             ),
             array(
                 'pergunta'=>'Esta autora teve seu rosto posto no fundo das notas de 100 Cruzados Novos,como forma de homenagem na época (fato que não permaneceu por muito tempo), 
                 foi a primeira mulher a ter um livro premiado pela academia Brasileira de Letras e sua última obra, denominada “Cânticos” foi publicado após sua morte.',
                 'opcoes'=> array(
-                'a'=>'Cecília Meireles',
-                'b'=>'Cora Coralina', 
-                'c'=>'Rachel de Queiroz',
-                'd'=>'Hilda Hilst'), 
-                'resposta' => 'a'
+                'Cecília Meireles',
+                'Cora Coralina', 
+                'Rachel de Queiroz',
+                'Hilda Hilst'), 
+                'resposta' => 'Cecília Meireles'
             ),
             array(
                 'pergunta'=>'Este autor escreveu os contos Caçador de camurças, Chronos Kai Anagke (Tempo e Destino, em grego), O mistério de Higmore Hall e Makiné. 
                 Todos foram levados a um concurso da revista O Cruzeiro e saíram vencedores.',
                 'opcoes'=> array(
-                'a'=>'Mário de Andrade',
-                'b'=>'José Evaristo', 
-                'c'=>'Guimarães Rosa',
-                'd'=>'Ariano Suassuna'), 
-                'resposta' => 'c'
+                'Mário de Andrade',
+                'José Evaristo', 
+                'Guimarães Rosa',
+                'Ariano Suassuna'), 
+                'resposta' => 'Guimarães Rosa'
             ),
             array(
                 'pergunta'=>'Esta autora começou a publicar os seus trabalhos quando tinha 76 anos, trabalhou como doceira enquanto levava a escrita como um hobby paralelo.',
                 'opcoes'=> array(
-                'a'=>'Rachel de Queiroz',
-                'b'=>'Cora Coralina', 
-                'c'=>'Hilda Hilst',
-                'd'=>'Carolina de Jesus'), 
-                'resposta' => 'b'
+                'Rachel de Queiroz',
+                'Cora Coralina', 
+                'Hilda Hilst',
+                'Carolina de Jesus'), 
+                'resposta' => 'Cora Coralina'
             ),
             array(
                 'pergunta'=>'Este autor foi escolhido por Machado de Assis para ser o Patrono da cadeira número 23 da Academia Brasileira de Letras. 
                 Ingressou no meio político, quando foi eleito como deputado no Ceará.',
                 'opcoes'=> array(
-                'a'=>'Graciliano Ramos',
-                'b'=>'Manuel Bandeira', 
-                'c'=>'Monteiro Lobato',
-                'd'=>'José de Alencar'), 
-                'resposta' => 'd'
+                'Graciliano Ramos',
+                'Manuel Bandeira', 
+                'Monteiro Lobato',
+                'José de Alencar'), 
+                'resposta' => 'José de Alencar'
             ),
             array(
                 'pergunta'=>'Este autor construiu um santuário ao ar livre. A construção foi feita em São José do Belmonte, no estado de Pernambuco, local onde ocorre a cavalgada 
                 inspirada em seu primeiro romance, Romance d’a pedra do reino. As três primeiras imagens do santuário são Jesus, Nossa Senhora e São José, que é o padroeiro do município.',
                 'opcoes'=> array(
-                'a'=>'Mário Quintana',
-                'b'=>'José Evaristo', 
-                'c'=>'Ariano Suassuna',
-                'd'=>'Manuel Bandeira'), 
-                'resposta' => 'c'
+                'Mário Quintana',
+                'José Evaristo', 
+                'Ariano Suassuna',
+                'Manuel Bandeira'), 
+                'resposta' => 'Ariano Suassuna'
             ),
             array(
                 'pergunta'=>'Aos 35 anos, esta autora passou a morar numa chácara em Campinas, a Casa do Sol, planejada com cuidado pela autora para ser um espaço de inspiração 
                 e criação artística. Cercada por cachorros, morou lá até o fim de sua vida e não parou de produzir.',
                 'opcoes'=> array(
-                'a'=>'Adélia Prado',
-                'b'=>'Ana Miranda', 
-                'c'=>'Carolina de Jesus',
-                'd'=>'Hilda Hilst'), 
+                'Adélia Prado',
+                'Ana Miranda', 
+                'Carolina de Jesus',
+                'Hilda Hilst'), 
                 'resposta' => 'd'
             )
         );
@@ -126,43 +126,14 @@
             }
         }
         $questionIndex++;
-
-        if ($questionIndex === $questoes) {?>
-            <table>
-                <tr>
-                    <td colspan="2">
-                    <?php 
-                            echo "<h1>Quiz completo!</h1>";
-                                echo "<p>Questões: $questoes</p>";
-                                echo "<p>Respostas corretas: $pontuacao</p>";
-                                echo "<p>Respostas incorretas " . ($questoes - $pontuacao) . "</p>";
-                                
-                                
-                                ?>
-                    </td>
-                </tr>
-            </table>
-           
-       <?php }
+         
     
     ?>
-    <table>
-    <?php if ($questionIndex === 0 || $questionIndex === $questoes) : ?>
         
-        <tr>
-            <td colspan="2">
-                <h1>Principais Escritores Brasileiros</h1>
-            </td>
-        </tr>
-        <tr>
-            <td colspan="2">
-                <form action="teste2.php" method="post">
-                    <input type="hidden" name="questionIndex" value="0">
-                    <input type="submit" name="jogar" value="Jogar" class="botao">
-                </form> 
-            </td>
-        </tr>
-        <?php elseif ($questionIndex < $questoes) : ?>
+    <table>
+    
+    
+        <?php if ($questionIndex < $questoes) : ?>
             <tr>
                 <td>
                 <h1>Principais Escritores Brasileiros</h1>
@@ -179,7 +150,7 @@
             <?php foreach ($quiz[$questionIndex]['opcoes'] as $opcao) : ?>
                 <tr>
                     <td colspan="2">
-                        <input type="radio" name="answer" value="<?php echo substr($opcao, 0, 1); ?>" required> <?php echo substr($opcao, 3); ?><br>
+                        <input type="radio" name="answer" value="<?php echo substr($opcao, 0, 0); ?>" required> <?php echo substr($opcao, 0); ?><br>
                         
                     </td>
                 </tr>
@@ -189,9 +160,41 @@
                         <input type="submit" name="submit" value="Continuar" class="botao">
                     </td>
                 </tr>
+                <?php elseif ($questionIndex === 0) : ?>
+        
+        <tr>
+            <td colspan="2">
+                <h1>Principais Escritores Brasileiros</h1>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <form action="teste2.php" method="post">
+                    <input type="hidden" name="questionIndex" value="0">
+                    <input type="submit" name="jogar" value="Jogar" class="botao">
+                </form> 
+            </td>
+        </tr>
+        <?php elseif($questionIndex === $questoes):?>
+                <tr>
+                    <td colspan="2">
+                     
+                            <h1>Quiz completo!</h1>
+                            <p>Questões: <?php $questoes ?></p>
+                            <p>Respostas corretas: <?php $pontuacao ?></p>
+                            <p>Respostas incorretas <?php ($questoes - $pontuacao) ?></p>
+                                
+                                
+                    </td>
+                </tr>
+                    <?php $questionIndex = 0;
+                    $pontuacao = 0; ?>
+       
+                
             
         </form>
         <?php endif; ?>
+        
     </table>
     
 </body>
